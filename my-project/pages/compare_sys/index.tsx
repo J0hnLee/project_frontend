955ad6fda8d68cfade2ed9ed9da5c0810d1a63bc
Lee Cheng Hsun <john791016@gmail.com>
2022-08-13 21:24:09 +0800

    add fun

--- my-project/pages/compare_sys/index.tsx
+++ my-project/pages/compare_sys/index.tsx
@@ -2,19 +2,12 @@ import ProTable from '@ant-design/pro-table';
 import { Popconfirm, Button } from 'antd';
 import { queryProductList } from '@/services/product';
 import React from 'react';
-import { ProForm, ProFormText } from '@ant-design/pro-components';
 
 
 const Comparesys = () => {
 
     return (
-        <div><ProForm
-        onFinish={async (values) => {
-          console.log(values);
-        }}
-      >
-        <ProFormText name="name" label="姓名" />
-      </ProForm></div> 
+        <div></div> 
         
     )
 }
